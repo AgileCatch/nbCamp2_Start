cf7ea6624aab799764071e584bb54c0f9a5918f7
jaewon <janga9710@naver.com>
2023-07-11 17:42:19 +0900

    [DOCS] : 📚

---
+++ lib/image_AddPage.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ServicePage(),
+    );
+  }
+}
+
+class ServicePage extends StatelessWidget {
+  const ServicePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: Row(
+          children: [
+            SizedBox(width: 16),
+            Text(
+              'back',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.normal,
+                fontSize: 17,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
