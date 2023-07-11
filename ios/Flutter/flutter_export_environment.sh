9af0b7eadd7371e6d5595c292a73dad582e468cc
yeonghyeon <agilecatch@gmail.com>
2023-07-11 23:40:14 +0900

    카드 만드는중

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/soohyun/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/soohyun/Desktop/study/flutter/nbCamp2_Start"
+export "FLUTTER_ROOT=/Users/gim-yeonghyeon/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/gim-yeonghyeon/Desktop/flutter/nbCamp2_Start"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/soohyun/Desktop/study/flutter/nbCamp2_Start/lib/main.dart"
+export "FLUTTER_TARGET=/Users/gim-yeonghyeon/Desktop/flutter/nbCamp2_Start/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -11,4 +11,4 @@ export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/soohyun/Desktop/study/flutter/nbCamp2_Start/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/gim-yeonghyeon/Desktop/flutter/nbCamp2_Start/.dart_tool/package_config.json"
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:introduce_myself/detail_ModifyPage.dart';
 import 'package:lottie/lottie.dart';
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
@@ -9,26 +11,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SplashScreen(), //스플래시 화면을 앱의첫화면으로 띄우기
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
+class SplashScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // 3초 후에 홈 화면으로 이동
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MyHomePage()),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,3 +59,76 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {
+              //추가버튼 클릭시 (detail_ModifyPage로 이동)
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => DetailModifyPage()),
+              );
+            },
+            icon: Icon(
+              Icons.add,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(14.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "이(E)와 아이(I)들",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            //카드위젯 만드는부분
+            Container(
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 255, 238, 186),
+                  borderRadius: BorderRadius.circular(30)),
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 170,
+                  vertical: 60,
+                ),
+              ),
+            ),
+            Center(
+              child: Text('Home Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+// SizedBox(
+              //   height: 130,
+              //   width: 360,
+              //   child: Card(
+              //     shape: ContinuousRectangleBorder(
+              //       borderRadius: BorderRadius.circular(60), //카드둥글기정도
+              //     ),
+              //   ),
+              // ),
