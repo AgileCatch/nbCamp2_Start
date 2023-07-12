46ed71dad55197b2408e93fa8cbecb735af415ef
yeonghyeon <agilecatch@gmail.com>
2023-07-12 10:38:57 +0900

    DESIGN] : 🎨

--- lib/Util/colorList.dart
+++ lib/Util/colorList.dart
@@ -6,5 +6,5 @@ class ColorList {
   Color box = Color.fromARGB(255, 209, 208, 208); //선 색
   Color yellow = Color.fromARGB(255, 255, 238, 186); //카드색
   Color orange = Color.fromARGB(255, 245, 122, 74);
-  Color background = Color.fromARGB(255, 230, 229, 229); //흰색바탕화면
+  Color background = Color.fromARGB(255, 255, 255, 255); //흰색바탕화면
 }
