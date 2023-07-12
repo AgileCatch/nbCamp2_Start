d37737e6ef2b4f17d04673f40631c7b7b38be603
SeoYeonMoon <mbb1223h@gmail.com>
2023-07-12 23:39:01 +0900

    third

--- lib/image_DetailPage.dart
+++ lib/image_DetailPage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:introduce_myself/image_Modifypage.dart';
+
+import 'detailPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +34,7 @@ class ImageDetailPage extends StatelessWidget {
             // 버튼 클릭시 이전 페이지로 이동
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (_) => ImageModifyPage()),
+              MaterialPageRoute(builder: (_) => MyPage()),
             );
           },
           icon:
@@ -153,83 +156,6 @@ class ImageDetailPage extends StatelessWidget {
                     ),
                   ),
                 ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-// 이미지 수정 및 저장 페이지
-class ImageModifyPage extends StatelessWidget {
-  ImageModifyPage({
-    super.key,
-  });
-
-  TextEditingController contentController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          onPressed: () {
-            // 버튼 클릭시 이전 페이지로 이동
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (_) => ImageDetailPage()),
-            );
-            Navigator.pop(context); // 팝업 닫기
-            Navigator.pop(context); //ImageDetailPage로 이동
-          },
-          icon:
-              Icon(Icons.backspace, color: Color.fromARGB(255, 136, 136, 136)),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              // 저장 버튼 클릭시
-            },
-            icon: Icon(Icons.check, color: Color.fromARGB(255, 136, 136, 136)),
-          )
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Container(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  width: 200,
-                  height: 60,
-                ),
-                SizedBox(height: 10),
-                Container(
-                  width: double.infinity,
-                  height: 300,
-                  child: Image.network(
-                    "http://news.samsungdisplay.com/wp-content/uploads/2018/08/8.jpg",
-                    width: double.infinity,
-                    height: 300,
-                  ),
-                ),
-                SizedBox(height: 0),
-                Container(
-                  height: 160,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: "내용을 입력하세요",
-                    ),
-                    maxLines: 7,
-                  ),
-                ),
-                SizedBox(height: 10),
-                Container(),
               ],
             ),
           ),
